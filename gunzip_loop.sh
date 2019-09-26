589eeae0838474e5b61b0c1f142bfa43b851c32a
Zeya Xue <ZXue@campus-002-005.ucdavis.edu>
2019-09-26 15:40:30 -0700

    mod back slashes

--- gunzip_loop.sh
+++ gunzip_loop.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #       0. Starting files location
-$starting_files_location=/share/lemaylab-backedup/Zeya/raw_data/NovaSeq043/tb957t8xg/Un_DTDB73/Project_DLZA_Nova043P_Alkan/
+$starting_files_location=/share/lemaylab-backedup/Zeya/raw_data/NovaSeq043/tb957t8xg/Un_DTDB73/Project_DLZA_Nova043P_Alkan
 
-$unzipped_files_output=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/unzipped/
+$unzipped_files_output=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/unzipped
 
 for file in $starting_files_location/*fastq.gz
 
 do 
-	gunzip -c $file > $unzipped_files_output
+	gunzip -c $file > $unzipped_files_output/
 
 done
