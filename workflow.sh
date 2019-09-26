f89f68609c551016e35d99636bb479fecb537a02
Zeya Xue <ZXue@Zeyas-MacBook-Pro.local>
2019-09-25 21:07:15 -0700

    MOD file path

--- workflow.sh
+++ workflow.sh
@@ -5,26 +5,27 @@
 # VARIABLES - set these paths for each step.
 #
 #       0. Starting files location
-starting_files_location=/path/to/stool_metagenomes/input_files/thirty_large_mg
+starting_files_location=/share/lemaylab-backedup/Zeya/raw_data/test_dataset
 
 #       1. Human Read Removal
-bmtagger_location=/path/to/bmtools/bmtagger
-bbmap_location=/path/to/Programs/BBMap/sh
-human_db=/path/to/databases/human_db
+bmtagger_location=/share/lemaylab-backedup/milklab/programs/bmtools/bmtagger
+# bbmap_location=/path/to/Programs/BBMap/sh 
+# 9/25/19 I can't find BBMap...Will dig for it later
+human_db=/share/lemaylab-backedup/milklab/michelle/databases/human_db
 
 #       2. PEAR
-pear_location=/path/to/Programs/pear-0.9.6/pear-0.9.6
+#pear_location=/path/to/Programs/pear-0.9.6/pear-0.9.6
 
 #       3. Trimmomatic
-trimmomatic_location=/path/to/Programs/Trimmomatic-0.33/trimmomatic-0.33.jar
+#trimmomatic_location=/path/to/Programs/Trimmomatic-0.33/trimmomatic-0.33.jar
 
 #       4. DIAMOND 
-b_galac_database="/path/to/protein_dbs/db_with_B-galac.dmnd"
-diamond_location="/path/to/Programs"
+#b_galac_database="/path/to/protein_dbs/db_with_B-galac.dmnd"
+#diamond_location="/path/to/Programs"
 
 #       5. Aggregation
-programs=/path/to/stool_metagenomes/Code
-B_galac_db="/path/to/golden_databases/protein_dbs/db_with_B-galac.faa"
+#programs=/path/to/stool_metagenomes/Code
+#B_galac_db="/path/to/golden_databases/protein_dbs/db_with_B-galac.faa"
 
 ####################################################################
 
@@ -34,17 +35,17 @@ B_galac_db="/path/to/golden_databases/protein_dbs/db_with_B-galac.faa"
 # Note: paired-end files are usually named using R1 and R2 in the name.
 # Note: if using single-end reads, only need to specify one input flag (-1)
 
-PATH=$PATH:/path/to/programs/bmtools/bmtagger
-PATH=$PATH:/path/to/programs/srprism/gnuac/app
+PATH=$PATH:/share/lemaylab-backedup/milklab/programs/bmtools/bmtagger
+PATH=$PATH:/share/lemaylab-backedup/milklab/programs/srprism/gnuac/app
 module load blast
 module load java bbmap
-echo "NOW STARTING master_beta.galac.db_analysis_stoolmg.sh AT: "; date
 echo "NOW STARTING HUMAN READ REMOVAL STEP AT: "; date
 
-for file in $starting_files_location/SRR5128401_pass_1.fastq
+for file in $starting_files_location/5052_S15_L003_R1_001.fastq
+
 do
         file1=$file
-        file2=$(echo $file1 | sed 's/pass_1/pass_2/')
+        file2=$(echo $file1 | sed 's/R1_001/R2_001/')
 		filename=$(basename "$file1")
         basename=$(echo $filename | cut -f 1 -d "_")
 		outname="$starting_files_location/$basename"
