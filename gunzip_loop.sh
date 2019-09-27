007ca0326101fd455a6fe1f8b044b0437cbf4f5f
Zeya Xue <ZXue@campus-114-065.ucdavis.edu>
2019-09-27 10:53:19 -0700

    change echo and action

--- gunzip_loop.sh
+++ gunzip_loop.sh
@@ -5,18 +5,17 @@ starting_files_location=/share/lemaylab-backedup/Zeya/raw_data/NovaSeq043/tb957t
 
 unzipped_files_output=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/unzipped
 
-
+# for loop to unzip all gz file to fastq in one file 
 for file in $starting_files_location/*fastq.gz
 do
 	STEM=$(basename "${file}" .gz)
 
 	if [ -f /$unzipped_files_output/"${STEM}" ]
 	then 
-		echo /$unzipped_files_output/"${STEM}".fastq
-		echo this file already exist and will not to rewrite
+		echo /$unzipped_files_output/"${STEM}".fastq already exist and will not be overwritten.
 	else
+		echo "${STEM}" does not exist. Unzipping now....		
 		gunzip -c "${file}" > /$unzipped_files_output/"${STEM}"
-		echo it does not exist. Being written now....
   	fi		
 done
 
