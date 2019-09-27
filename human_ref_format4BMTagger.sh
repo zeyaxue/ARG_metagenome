18cffd77cdf4b4928381622725b7d500a1a1d994
Zeya Xue <ZXue@campus-114-065.ucdavis.edu>
2019-09-27 14:04:37 -0700

    add check sum

--- human_ref_format4BMTagger.sh
+++ human_ref_format4BMTagger.sh
@@ -1,10 +1,22 @@
-#### Created by Zeya Xue on 09.27.2019
-#
-############################################################
+#!/bin/sh
 
+#### Created by Zeya Xue on 09.27.2019
+
+
+############################################################
 # 1. download the most up to date human reference genome from NCBI RefSeq
 ######This only needs to be run one time for downloading. If you have already downloaded the ref genome, proceed to step 2 directly
 
 # download script from https://www.ncbi.nlm.nih.gov/genome/doc/ftpfaq/
-rsync --copy-links --recursive --times --verbose rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.39_GRCh38.p13 /share/lemaylab-backedup/milklab/database/human_GRCh38_p13
+#rsync --copy-links --recursive --times --verbose rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.39_GRCh38.p13 /share/lemaylab-backedup/milklab/database/human_GRCh38_p13
 
+# check sums to make sure that the .gz sequence files are completely download
+db_path=/share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13
+cksum_output=/share/lemaylab-backedup/milklab/database/human_GRCh38_p13/
+
+for file in $db_path*.gz; do
+	md5sum $file >> $cksum_output/computed_check_sums.txt
+	sed -i "s|$path|./|g" $cksum_output/computed_check_sums.txt
+done
+
+diff $cksum_output/computed_check_sums.txt /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13/md5checksums.txt >> /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/diff.txt
