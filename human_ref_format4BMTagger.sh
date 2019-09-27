bf6b7fde44520902f78557c51aec9c5055a5a51a
Zeya Xue <ZXue@campus-114-065.ucdavis.edu>
2019-09-27 14:26:55 -0700

    mod

--- human_ref_format4BMTagger.sh
+++ human_ref_format4BMTagger.sh
@@ -12,13 +12,13 @@
 
 # check sums to make sure that the .gz sequence files are completely download
 db_path=/share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13
-cksum_output=/share/lemaylab-backedup/milklab/database/human_GRCh38_p13/
+cksum_output=/share/lemaylab-backedup/milklab/database/human_GRCh38_p13/computed_check_sums.txt
 
 for file in $db_path/*.gz; do
-	md5sum $file >> $cksum_output/computed_check_sums.txt
-	sed -i "s|$path|./|g" "$cksum_output/computed_check_sums.txt"
+	md5sum $file >> $cksum_output
+	sed -i "s|$path|./|g" $cksum_output
 done
 
-diff $cksum_output/computed_check_sums.txt /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13/md5checksums.txt >> /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/diff.txt
+diff $cksum_output /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13/md5checksums.txt >> /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/diff.txt
 # move to my github account so I can see it more easily
 cp /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/diff.txt /share/lemaylab-backedup/Zeya/scripts/gitSRC/ARG_metagenome/diff.txt
