2d01a8a412eea852715f9193b8755e4be61ffe65
Zeya Xue <ZXue@campus-114-065.ucdavis.edu>
2019-09-27 15:53:55 -0700

    Add format commands

--- human_ref_format4BMTagger.sh
+++ human_ref_format4BMTagger.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#SBATCH --mem-per-cpu 2000
+#SBATCH --mail-user=mltreiber@ucdavis.edu
+#SBATCH --mail-type=END
 
 #### Created by Zeya Xue on 09.27.2019
 
@@ -14,11 +17,31 @@
 db_path=/share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13
 cksum_output=/share/lemaylab-backedup/milklab/database/human_GRCh38_p13/computed_check_sums.txt
 
-for file in $db_path/*.gz; do
-	md5sum $file >> $cksum_output
-	sed -i "s|$db_path||g" $cksum_output
-done
+#for file in $db_path/*.gz; do
+#	md5sum $file >> $cksum_output
+#	sed -i "s|$db_path||g" $cksum_output
+#done
 
-diff $cksum_output /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13/md5checksums.txt >> /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/diff.txt
+#diff $cksum_output /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13/md5checksums.txt >> /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/diff.txt
+
 # move to my github account so I can see it more easily
-cp /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/diff.txt /share/lemaylab-backedup/Zeya/scripts/gitSRC/ARG_metagenome/diff.txt
+# cp /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/diff.txt /share/lemaylab-backedup/Zeya/scripts/gitSRC/ARG_metagenome/diff.txt
+
+############################################################
+# 2. format reference
+bmtool_location=/share/milklab/programs/bmtools/bmtagger/bmtool
+srprism_location=/share/milklab/programs/srprism/gnuac/app/srprism
+
+# unzip the zipped ref genome
+gunzip -c /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13/GCF_000001405.39_GRCh38.p13_genomic.fna.gz > /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13_genomic.fna
+
+ref_file=/share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13_genomic.fna
+
+# Make index for bmfilter
+$bmtool_location -d $ref_file -o /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13_genomic.bitmask -A 0 -w 18
+
+# Make index for srprism
+$srprism_location mkindex -i $ref_file -o /share/lemaylab-backedup/milklab/database/human_GRCh38_p13/GCF_000001405.39_GRCh38.p13_genomic.srprism -M 7168
+
+
+
