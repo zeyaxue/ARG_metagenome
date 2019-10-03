f5b1f365e0b0b80fbedce6a4d471020bace193bf
Zeya Xue <zhxue@ucdavis.edu>
2019-10-03 11:52:01 -0700

    Finish install fastuniq

--- install_FastUniq.sh
+++ install_FastUniq.sh
@@ -3,18 +3,22 @@
 # I followed the link below to install minoconda
 # https://bioconda.github.io/user/install.html#set-up-channels
 # I downloaded and installed the minoconda to my directory on spitfire
-#cd /share/lemaylab-backedup/Zeya
+cd /share/lemaylab-backedup/Zeya # Only needed for first-time setup
 
 #wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-#sh Miniconda3-latest-Linux-x86_64.sh 
+sh Miniconda3-latest-Linux-x86_64.sh 
 
 # Miniconda 3 is installed into this location: /home/xzyao/miniconda3
-# Add this path to the environmental variable $PATH so conda command can be called
+# Add this path to the environmentals variable $PATH so conda command can be called
 # Note: this is only changing the PATH temporarily. 
 # For permant change, one can add the command to .bash_profile (but I won't do in this case to not mess with other scripts)
 export PATH="/home/xzyao/miniconda3/bin:$PATH"
 
 # set up channels, add bioconda channel and other dependencies
-conda config --add channels defaults
-conda config --add channels bioconda
-conda config --add channels conda-forge
+conda config --add channels defaults # Only needed for first-time setup
+conda config --add channels bioconda # Only needed for first-time setup
+conda config --add channels conda-forge # Only needed for first-time setup
+
+# install FastUniq package
+conda install fastuniq
+# The version I got is fastuniq-1.1-h470a237_1
