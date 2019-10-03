87e78309b909db60e8001e0d743427528743ea84
Zeya Xue <xzyao@spitfire.hpc.genomecenter.ucdavis.edu>
2019-10-03 10:26:29 -0700

    chmod

--- install_FastUniq.sh
+++ install_FastUniq.sh
@@ -12,7 +12,7 @@
 # Add this path to the environmental variable $PATH so conda command can be called
 # Note: this is only changing the PATH temporarily. 
 # For permant change, one can add the command to .bash_profile (but I won't do in this case to not mess with other scripts)
-export PATH="/home/xzyao/miniconda3:$PATH"
+export PATH="/home/xzyao/miniconda3/bin:$PATH"
 
 # set up channels, add bioconda channel and other dependencies
 conda config --add channels defaults
