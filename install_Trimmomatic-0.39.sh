51335fa34dc9281fb3571db1a9139b83f1f9100e
Zeya Xue <zhxue@ucdavis.edu>
2019-10-04 16:26:12 -0700

    ADD file

---
+++ install_Trimmomatic-0.39.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# I got this link by going to the website, and right click to copy link address
+cd /share/lemaylab-backedup/milklab/programs
+wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-Src-0.39.zip
+unzip Trimmomatic-Src-0.39.zip
+rm Trimmomatic-Src-0.39.zip
