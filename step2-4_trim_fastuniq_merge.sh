9a364bf8eccfbe93369db88a5813d571760b6a89
Zeya Xue <zhxue@ucdavis.edu>
2019-10-05 14:51:24 -0700

    ADD file

---
+++ step2-4_trim_fastuniq_merge.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --mail-user=zhxue@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=trim_Fastuniq_merge
+#SBATCH --cpus-per-task 7
+
+#       3. Trimmomatic
+trimmomatic_location=/share/lemaylab-backedup/milklab/programs/
+
+###################################################################
+#
+# STEP 2: USE TRIMMOMATIC TO REMOVE LOW-QUALITY READS
+# Take input from BMTagger (removal of the human DNA)
+# Trimmomatic parameters followed this paper (Taft et al., 2018, mSphere), using the paired end mode 
+
+echo "NOW STARTING READ CLEANING WITH TRIMMOMATIC AT: "; date 
+
+module load java trimmomatic
+
+input_dir=/share/lemaylab-backedup/Zeya/proceesed_data/test_no_humuan_dataset
+mkdir /share/lemaylab-backedup/Zeya/proceesed_data/step2_trim/
+output_dir=/share/lemaylab-backedup/Zeya/proceesed_data/step2_trim/
+
+for file in $input_dir/*R1_nohuman_1000* 
+do
+	STEM=$(basename "${file}" 1_nohuman_1000reads.fastq)
+
+	file1=$file
+	file2=$STEM2_nohuman_1000reads.fastq
+	echo $file2
+
+	#java -jar $trimmomatic_location PE $file1 $file2 $STEM
+	#-phred33 $file $shortname SLIDINGWINDOW:4:15 MINLEN:99
+
+
