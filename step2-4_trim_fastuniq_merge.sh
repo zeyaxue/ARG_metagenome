49290499b44fb2b54761b4a58de2aa51bed29910
Zeya Xue <zhxue@ucdavis.edu>
2019-10-05 15:37:50 -0700

    remove echo debug

--- step2-4_trim_fastuniq_merge.sh
+++ step2-4_trim_fastuniq_merge.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task 7
 
 #       3. Trimmomatic
-trimmomatic_location=/share/lemaylab-backedup/milklab/programs/
+trimmomatic_location=/home/xzyao/miniconda3/pkgs/trimmomatic-0.39-1/bin
 
 ###################################################################
 #
