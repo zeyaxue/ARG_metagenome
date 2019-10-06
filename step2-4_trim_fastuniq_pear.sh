43b661bf1fdd15a3f2fd4c1304972b31b5bf8737
Zeya Xue <zhxue@ucdavis.edu>
2019-10-06 09:43:58 -0700

    rename merge step to pear

--- step2-4_trim_fastuniq_pear.sh
+++ step2-4_trim_fastuniq_pear.sh
@@ -23,18 +23,18 @@ echo "NOW STARTING READ CLEANING WITH TRIMMOMATIC AT: "; date
 
 module load java trimmomatic
 
-input_dir_trim=/share/lemaylab-backedup/Zeya/proceesed_data/test_no_humuan_dataset
-#mkdir /share/lemaylab-backedup/Zeya/proceesed_data/step2_trim/ # only need to run once 
-output_dir_trim=/share/lemaylab-backedup/Zeya/proceesed_data/test_no_humuan_dataset/step2_trim
+input_dir_trim=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step_1_BMTagger_output
+mkdir /share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step2_trim/ # only need to run once 
+output_dir_trim=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step2_trim
 
-for file in $input_dir_trim/*R1_nohuman_1000* 
+for file in $input_dir_trim/500*R1_nohuman.fastq # test with 4 files first (5002, 5005, 5006, 5007)
 do
-	STEM=$(basename "${file}" 1_nohuman_1000reads.fastq)
+	STEM=$(basename "${file}" 1_nohuman.fastq)
 
 	file1=$file
-	file2=$input_dir_trim/${STEM}2_nohuman_1000reads.fastq
+	file2=$input_dir_trim/${STEM}2_nohuman.fastq
 
-	java -jar $trimmomatic_location PE -threads 5 -trimlog output_dir_trim/trimmomatic_log.txt $file1 $file2 $output_dir_trim/${STEM}1_1000reads_paired.fastq $output_dir_trim/${STEM}1_1000reads_unpaired.fastq.gz $output_dir_trim/${STEM}2_1000reads_paired.fastq $output_dir_trim/${STEM}2_1000reads_unpaired.fastq.gz -phred33 SLIDINGWINDOW:4:15 MINLEN:99
+	java -jar $trimmomatic_location PE -threads 5 -trimlog $output_dir_trim/trimmomatic_log.txt $file1 $file2 $output_dir_trim/${STEM}1_paired.fastq $output_dir_trim/${STEM}1_unpaired.fastq.gz $output_dir_trim/${STEM}2_paired.fastq $output_dir_trim/${STEM}2_unpaired.fastq.gz -phred33 SLIDINGWINDOW:4:15 MINLEN:99
 done
 
 
@@ -46,18 +46,18 @@ done
 
 echo "NOW STARTING REMOVING DUPLICATE READS AT: "; date 
 
-#mkdir /share/lemaylab-backedup/Zeya/proceesed_data/test_no_humuan_dataset/step3_fastuniq
-output_dir_dup=/share/lemaylab-backedup/Zeya/proceesed_data/test_no_humuan_dataset/step3_fastuniq
+mkdir /share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step3_fastuniq
+output_dir_dup=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step3_fastuniq
 
 touch $output_dir_dup/fastuniq_input_list.txt
 fastuniq_input_list=$output_dir_dup/fastuniq_input_list.txt
 
-for file in $output_dir_trim/*R1_1000reads_paired.fastq
+for file in $output_dir_trim/*R1_paired.fastq
 do 
-	STEM=$(basename "${file}" 1_1000reads_paired.fastq)
+	STEM=$(basename "${file}" 1_paired.fastq)
 
 	file1=$file
-	file2=$output_dir_trim/${STEM}2_1000reads_paired.fastq
+	file2=$output_dir_trim/${STEM}2_paired.fastq
 
 	echo $file1 >> $fastuniq_input_list
 	echo $file2 >> $fastuniq_input_list
@@ -65,7 +65,7 @@ do
 	# Althoug FastUniq takes list as input !!!!DO NOT SUPPLY ALL FILES IN ONE LIST!!!!
 	# Because FastUniq can only write out 2 files for forward and reverse
 	# Run FastUniq
-	$fastuniq_location -i $fastuniq_input_list -t q -o $output_dir_dup/${STEM}1_100reads_dup.fastq -p $output_dir_dup/${STEM}2_100reads_dup.fastq
+	$fastuniq_location -i $fastuniq_input_list -t q -o $output_dir_dup/${STEM}1_dup.fastq -p $output_dir_dup/${STEM}2_dup.fastq
 
 	# clear the content of the list file so that FastUniq takes only 2 files every time it runs
 	> $fastuniq_input_list
@@ -81,15 +81,15 @@ done
 
 echo "NOW STARTING PAIRED-END MERGING WITH PEAR AT: "; date
 
-#mkdir /share/lemaylab-backedup/Zeya/proceesed_data/test_no_humuan_dataset/step4_pear
-output_dir_pear=/share/lemaylab-backedup/Zeya/proceesed_data/test_no_humuan_dataset/step4_pear
+mkdir /share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step4_pear
+output_dir_pear=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step4_pear
 
-for file in $output_dir_dup/*1_100reads_dup.fastq
+for file in $output_dir_dup/*1_dup.fastq
 do
-	STEM=$(basename "${file}" 1_100reads_dup.fastq)
+	STEM=$(basename "${file}" R1_dup.fastq)
 
 	file1=$file
-	file2=$output_dir_dup/${STEM}2_100reads_dup.fastq
+	file2=$output_dir_dup/${STEM}R2_dup.fastq
 
 	$pear_location -f $file1 -r $file2 -o $output_dir_pear/${STEM} -j 5 # run on 5 threads
 done
