d596495dcbba87cbc09408bd47e66f082d83c8cf
Zeya Xue <zhxue@ucdavis.edu>
2019-10-05 23:54:16 -0700

    add head method to subset reads

--- subset_reads.sh
+++ subset_reads.sh
@@ -1,6 +1,8 @@
 starting_files_location=/share/lemaylab-backedup/Zeya/raw_data/test_dataset
 
-#/share/lemaylab-backedup/Zeya/programs/seqtk/seqtk/seqtk sample -s100 $starting_files_location/step_1_BMTagger_output/5052.R1_nohuman.fastq 1000 > $starting_files_location/step_1_BMTagger_output/5052.R1_nohuman_1000reads.fastq
+/share/lemaylab-backedup/Zeya/programs/seqtk/seqtk/seqtk sample -s100 $starting_files_location/step_1_BMTagger_output/5052.R1_nohuman.fastq 1000 > $starting_files_location/step_1_BMTagger_output/5052.R1_nohuman_1000reads.fastq
 /share/lemaylab-backedup/Zeya/programs/seqtk/seqtk/seqtk sample -s100 $starting_files_location/step_1_BMTagger_output/5052.R2_nohuman.fastq 1000 > $starting_files_location/step_1_BMTagger_output/5052.R2_nohuman_1000reads.fastq
  
 
+# or used the head command
+head -n 4000 /share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step_1_BMTagger_output/6095.R2_nohuman.fastq > ../6095.R2_nohuman_1000reads.fastq
