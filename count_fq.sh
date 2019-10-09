a89b93ebf59afc4b9546a392a21674ea1d90853f
Zeya Xue <zhxue@ucdavis.edu>
2019-10-09 11:05:39 -0700

    modify file list counted

--- count_fq.sh
+++ count_fq.sh
@@ -4,9 +4,9 @@
 read -p "Enter input directory: " indir
 touch $indir/count_fq_log.txt
 
-for file in $indir/*..assembled.fastq
+for file in $indir/500*.fastq
 do 
-	STEM=$(basename "${file}" ..assembled.fastq)
+	STEM=$(basename "${file}" .fastq)
 	echo ${STEM} >> $indir/count_fq_log.txt
  
 	echo $(cat $file|wc -l)/4|bc >> $indir/count_fq_log.txt
