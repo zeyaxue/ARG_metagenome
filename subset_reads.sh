fe499f58a30cb6d98902cfd491a6835055c267b1
Zeya Xue <zhxue@ucdavis.edu>
2019-10-10 16:20:17 -0700

    MOD to customized in/out

--- subset_reads.sh
+++ subset_reads.sh
@@ -1,8 +1,17 @@
-starting_files_location=/share/lemaylab-backedup/Zeya/raw_data/test_dataset
+#!/bin/bash
 
-/share/lemaylab-backedup/Zeya/programs/seqtk/seqtk/seqtk sample -s100 $starting_files_location/step_1_BMTagger_output/5052.R1_nohuman.fastq 1000 > $starting_files_location/step_1_BMTagger_output/5052.R1_nohuman_1000reads.fastq
-/share/lemaylab-backedup/Zeya/programs/seqtk/seqtk/seqtk sample -s100 $starting_files_location/step_1_BMTagger_output/5052.R2_nohuman.fastq 1000 > $starting_files_location/step_1_BMTagger_output/5052.R2_nohuman_1000reads.fastq
+# Take input directory 
+read -p "Enter input directory: " indir
+# Enter output directory
+read -p "Enter output directory: " outdir
+
+#/share/lemaylab-backedup/Zeya/programs/seqtk/seqtk/seqtk sample -s100 $starting_files_location/step_1_BMTagger_output/5052.R1_nohuman.fastq 1000 > $starting_files_location/step_1_BMTagger_output/5052.R1_nohuman_1000reads.fastq
+#/share/lemaylab-backedup/Zeya/programs/seqtk/seqtk/seqtk sample -s100 $starting_files_location/step_1_BMTagger_output/5052.R2_nohuman.fastq 1000 > $starting_files_location/step_1_BMTagger_output/5052.R2_nohuman_1000reads.fastq
  
 
 # or used the head command
-head -n 4000 /share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step_1_BMTagger_output/6095.R2_nohuman.fastq > ../6095.R2_nohuman_1000reads.fastq
+for file in $indir/500*.fastq 
+do
+	STEM=$(basename "${file}" .fastq)
+	head -n 4000 $file > $outdir/${STEM}_1000reads.fastq
+done
