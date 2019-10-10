4d8ad53153976c515d471111dfeb41744142cce9
Zeya Xue <zhxue@ucdavis.edu>
2019-10-10 15:26:22 -0700

    ADD part to format output file

--- count_fq.sh
+++ count_fq.sh
@@ -3,12 +3,48 @@
 # Take input directory 
 read -p "Enter input directory: " indir
 touch $indir/count_fq_log.txt
+# Determine if input is fastq or gz
+read -p "Enter fq if input is fastq. Enter gz if input is zipped fastq: " format
 
-for file in $indir/500*.fastq
-do 
-	STEM=$(basename "${file}" .fastq)
-	echo ${STEM} >> $indir/count_fq_log.txt
- 
-	echo $(cat $file|wc -l)/4|bc >> $indir/count_fq_log.txt
-done
 
+if [ "$format" = "fq" ]
+then 
+	for file in $indir/*.fastq
+	do 
+		STEM=$(basename "${file}" .fastq)
+		echo ${STEM} >> $indir/count_fq_log.txt	
+	 
+		echo $(cat $file|wc -l)/4|bc >> $indir/count_fq_log.txt
+	done
+elif [ "$format" = "gz" ]
+then
+	for file in $indir/*.gz
+	do 
+		STEM=$(basename "${file}" .fastq.gz )
+		echo ${STEM} >> $indir/count_fq_log.txt	
+	 
+		echo $(zcat $file|wc -l)/4| >> $indir/count_fq_log.txt
+	done	
+else 
+	echo "Please input between fq and gz" 
+fi
+
+# organize file and move to new location
+#mv $indir/count_fq_log.txt /share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq072/count_fq_log.txt
+cd /share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq072/
+
+# Make sampleID column
+## I tried over several hours to wrangle the data in variables but that did not work
+awk 'NR%2==1' count_fq_log.txt > col1.txt  # use $() to denote everthing between () are excutable commands
+awk 'NR%2==1' col1.txt > col1a.txt  # because every sample name is listed twice 
+$(echo sampleID > col1b.txt) # column header
+$(sed 's/_S.*//' col1a.txt >> col1b.txt) # need the . period symbol for wildcard to work
+
+
+# Make read column
+awk 'NR%2==0' count_fq_log.txt > col2.txt
+$(echo lib_size > col2a.txt)
+$(awk 'NR%2==1' col2.txt >> col2a.txt)
+
+$(paste col1b.txt col2a.txt -d "\t" > lib_size_NovaSeq_072.txt)
+rm col*
