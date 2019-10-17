8fa6cdcc0e703db05641fba7971cdefd59ca0903
Zeya Xue <zhxue@ucdavis.edu>
2019-10-17 12:11:36 -0700

    add files

---
+++ install_ResFinder_db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+cd /share/lemaylab-backedup/milklab/database
+git clone https://git@bitbucket.org/genomicepidemiology/resfinder_db.git
+
+mv resfinder_db/ resfinder_db-3.2_Oct2019/
+
+
+# Make this database compatible witH DIAMOND
