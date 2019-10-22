5f6fd810420150037be744ef00f3b8ea3a7a396d
Zeya Xue <zhxue@ucdavis.edu>
2019-10-22 09:03:02 -0700

    modify to take positional arg input

--- count_fq.sh
+++ count_fq.sh
@@ -1,49 +1,46 @@
 #!/bin/bash
 
-# Take input directory 
-read -p "Enter input directory: " indir
-touch $indir/count_fq_log.txt
-# Determine if input is fastq or gz
-read -p "Enter fq if input is fastq. Enter gz if input is zipped fastq: " format
+# usage: source count_fq.sh indir outfile gz/fq
 
+touch $1/count_fq_log.txt
 
-if [ "$format" = "fq" ]
+if [ "$3" = "fq" ]
 then 
-	for file in $indir/*.fastq
+	for file in $1/*.fastq
 	do 
 		STEM=$(basename "${file}" .fastq)
-		echo ${STEM} >> $indir/count_fq_log.txt	
+		echo ${STEM} >> $1/count_fq_log.txt	
 	 
-		echo $(cat $file|wc -l)/4|bc >> $indir/count_fq_log.txt
+		echo $(cat $file|wc -l)/4|bc >> $1/count_fq_log.txt
 	done
-elif [ "$format" = "gz" ]
+elif [ "$3" = "gz" ]
 then
-	for file in $indir/*.gz
+	for file in $1/*.gz
 	do 
 		STEM=$(basename "${file}" .fastq.gz )
-		echo ${STEM} >> $indir/count_fq_log.txt	
+		echo ${STEM} >> $1/count_fq_log.txt	
 	 
-		echo $(zcat $file|wc -l)/4| >> $indir/count_fq_log.txt
+		echo $(zcat $file|wc -l)/4| >> $1/count_fq_log.txt
 	done	
 else 
 	echo "Please input between fq and gz" 
 fi
 
 # organize file
-cd $indir
+cd $1
 
 # Make sampleID column
 ## I tried over several hours to wrangle the data in variables but that did not work
-awk 'NR%2==1' count_fq_log.txt > col1.txt  # use $() to denote everthing between () are excutable commands
-awk 'NR%2==1' col1.txt > col1a.txt  # because every sample name is listed twice 
+awk 'NR%2==1' count_fq_log.txt > col1a.txt  # use $() to denote everthing between () are excutable commands
+#awk 'NR%2==1' col1.txt > col1a.txt  # because every sample name is listed twice 
 $(echo sampleID > col1b.txt) # column header
 $(sed 's/_S.*//' col1a.txt >> col1b.txt) # need the . period symbol for wildcard to work
 
 
 # Make read column
-awk 'NR%2==0' count_fq_log.txt > col2.txt
 $(echo lib_size > col2a.txt)
-$(awk 'NR%2==1' col2.txt >> col2a.txt)
+awk 'NR%2==0' count_fq_log.txt >> col2a.txt
+#$(awk 'NR%2==1' col2.txt >> col2a.txt)# because every sample name is listed twice 
 
-$(paste col1b.txt col2a.txt -d "\t" > lib_size.txt)
+$(paste col1b.txt col2a.txt -d "\t" > $2)
 rm col*
