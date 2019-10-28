d81124c0d4d003f62b777b6607a547b28fa5ac36
Zeya Xue <zhxue@ucdavis.edu>
2019-10-28 11:44:23 -0700

    Index database

--- init/install_MEGARes_v2.sh
+++ init/install_MEGARes_v2.sh
@@ -8,3 +8,9 @@ unzip megares_v2.00.zip
 
 mkdir megares_v2
 mv megares_* megares_v2/
+
+# index the database for bwa
+module load bwa
+
+bwa index megares_v2/megares_modified_database_v2.00.fasta 
+bwa index megares_v2/megares_drugs_database_v2.00.fasta
