ab62671b1a42501598bbc5d12647bd0807ca1fad
Zeya Xue <zhxue@ucdavis.edu>
2019-10-30 14:18:18 -0700

    Add files

---
+++ init/install_Rarefaction_Analyzer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+cd /share/lemaylab-backedup/milklab/programs/
+
+git clone https://github.com/cdeanj/RarefactionAnalyzer.git
+cd RarefactionAnalyzer
+make
---
+++ init/install_Resistome_Analyzer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+# Github repo: https://github.com/cdeanj/resistomeanalyzer
+
+cd /share/lemaylab-backedup/milklab/programs/
+git clone https://github.com/cdeanj/resistomeanalyzer.git
+cd resistomeanalyzer/
+
+make
