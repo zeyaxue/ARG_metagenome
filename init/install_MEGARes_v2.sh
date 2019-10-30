9a084575658d8d19d12ace6a919248bbd5a18c2d
Zeya Xue <zhxue@ucdavis.edu>
2019-10-30 14:27:40 -0700

    Modify

--- init/install_MEGARes_v2.sh
+++ init/install_MEGARes_v2.sh
@@ -14,3 +14,16 @@ module load bwa
 
 bwa index megares_v2/megares_modified_database_v2.00.fasta 
 bwa index megares_v2/megares_drugs_database_v2.00.fasta
+
+# caculate the gene length for all genes 
+# This info is needed for MicrobeCensus normalization
+cd megares_v2/
+
+for file in *.fasta
+do
+	STEM=$(basename "$file" .fasta)
+
+	source /share/lemaylab-backedup/Zeya/scripts/gitSRC/ARG_metagenome/count_fasta_length.sh \
+	$file \
+	${STEM}_GeneLen.txt 
+done	
