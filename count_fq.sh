749d2559e3a7babfdd0696d9dbfc3f8bd3a1981a
zeyaxue <zhxue@ucdavis.edu>
2019-11-18 14:01:33 -0800

    Modify input parameters

--- count_fq.sh
+++ count_fq.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
 
-# usage: source count_fq.sh indir outfile gz/fq
+# usage: source count_fq.sh gz/fq indir outfile    
 
-touch $1/count_fq_log.txt
+touch $2/count_fq_log.txt
 
-if [ "$3" = "fq" ]
+# get the 
+
+
+if [ "$1" = "fq" ]
 then 
-	for file in $1/*.fastq
+	for file in $2/*.fastq
 	do 
-		STEM=$(basename "${file}" .fastq)
-		echo ${STEM} >> $1/count_fq_log.txt	
+		STEM=$(basename "$file" .fastq)
+		echo ${STEM} >> $2/count_fq_log.txt	
 	 
-		echo $(cat $file|wc -l)/4|bc >> $1/count_fq_log.txt
+		echo $(cat $file|wc -l)/4|bc >> $2/count_fq_log.txt
 	done
-elif [ "$3" = "gz" ]
+elif [ "$1" = "gz" ]
 then
-	for file in $1/*.gz
+	for file in $2/*.fastq.gz
 	do 
-		STEM=$(basename "${file}" .fastq.gz )
-		echo ${STEM} >> $1/count_fq_log.txt	
+		STEM=$(basename "$file" .fastq.gz )
+		echo ${STEM} >> $2/count_fq_log.txt	
 	 
-		echo $(zcat $file|wc -l)/4| >> $1/count_fq_log.txt
+		echo $(zcat $file|wc -l)/4| >> $2/count_fq_log.txt
 	done	
 else 
 	echo "Please input between fq and gz" 
 fi
 
 # organize file
-cd $1
+cd $2
 
 # Make sampleID column
 ## I tried over several hours to wrangle the data in variables but that did not work
 awk 'NR%2==1' count_fq_log.txt > col1a.txt  # use $() to denote everthing between () are excutable commands
-#awk 'NR%2==1' col1.txt > col1a.txt  # because every sample name is listed twice 
 $(echo sampleID > col1b.txt) # column header
-$(sed 's/_S.*//' col1a.txt >> col1b.txt) # need the . period symbol for wildcard to work
-
+$(sed 's/_.*//' col1a.txt >> col1b.txt) # need the . period symbol for wildcard to work
 
 # Make read column
-$(echo lib_size > col2a.txt)
-awk 'NR%2==0' count_fq_log.txt >> col2a.txt
-#$(awk 'NR%2==1' col2.txt >> col2a.txt)# because every sample name is listed twice 
+$(echo lib_size > col2.txt)
+awk 'NR%2==0' count_fq_log.txt >> col2.txt
 
-$(paste col1b.txt col2a.txt -d "\t" > $2)
-rm col*
+# Combine 2 columns to final stats file
+$(paste col1b.txt col2.txt -d "\t" > $3)
+
