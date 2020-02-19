9fb0f1869246fe5ef88169d98c3171566c9bb338
zeyaxue <zhxue@ucdavis.edu>
2020-02-19 14:43:00 -0800

    Modify files

--- count_fq.sh
+++ count_fq.sh
@@ -34,14 +34,14 @@ cd $2
 
 # Make sampleID column
 ## I tried over several hours to wrangle the data in variables but that did not work
-awk 'NR%2==1' count_fq_log.txt > col1a.txt  # use $() to denote everthing between () are excutable commands
-$(echo sampleID > col1b.txt) # column header
-$(sed 's/_.*//' col1a.txt >> col1b.txt) # need the . period symbol for wildcard to work
+awk 'NR%2==1' $2/count_fq_log.txt > $2/col1a.txt  # use $() to denote everthing between () are excutable commands
+$(echo sampleID > $2/col1b.txt) # column header
+$(sed 's/_.*//' $2/col1a.txt >> $2/col1b.txt) # need the . period symbol for wildcard to work
 
 # Make read column
-$(echo lib_size > col2.txt)
-awk 'NR%2==0' count_fq_log.txt >> col2.txt
+$(echo lib_size > $2/col2.txt)
+awk 'NR%2==0' $2/count_fq_log.txt >> $2/col2.txt
 
 # Combine 2 columns to final stats file
-$(paste col1b.txt col2.txt -d "\t" > $3)
+$(paste $2/col1b.txt $2/col2.txt -d "\t" > $3)
 
