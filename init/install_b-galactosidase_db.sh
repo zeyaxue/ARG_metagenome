780b925f9dc8ce9eb43522fbd3c9478f7cc7b6a7
zeyaxue <zhxue@ucdavis.edu>
2020-03-21 20:10:53 -0700

    Modify files

--- init/install_b-galactosidase_db.sh
+++ init/install_b-galactosidase_db.sh
@@ -11,9 +11,11 @@
 # More on DIAMOND: https://github.com/bbuchfink/diamond
 
 # setting general variables:
-diamond_location=/share/lemaylab-backedup/milklab/programs/diamond
+#diamond_location=/share/lemaylab-backedup/milklab/programs/diamond
 bgdb=/share/lemaylab-backedup/databases/b-galactosidase/db_with_B-galac.faa
+#
+## --in: Path to the input protein reference database file in FASTA format
+## --db: Path to the output DIAMOND database file.
+#$diamond_location makedb --in $bgdb --db /share/lemaylab-backedup/databases/b-galactosidase/db_with_B-galac
 
-# --in: Path to the input protein reference database file in FASTA format
-# --db: Path to the output DIAMOND database file.
-$diamond_location makedb --in $bgdb --db /share/lemaylab-backedup/databases/b-galactosidase/db_with_B-galac
+python /share/lemaylab-backedup/milklab/michelle/functional_db_comparison/Code/python_scripts/get_all_families_mlt.py -db $bgdb -O /share/lemaylab-backedup/databases/b-galactosidase/B-galac_family_names.tsv
