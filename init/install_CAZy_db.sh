e7b5dc6cd42f2eb767a05f27e99c638baba8ea9d
zeyaxue <zhxue@ucdavis.edu>
2020-03-21 20:09:48 -0700

    Remove repeated header and seqs

--- init/install_CAZy_db.sh
+++ init/install_CAZy_db.sh
@@ -16,4 +16,36 @@ cazydb=/share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.fa
 
 # --in: Path to the input protein reference database file in FASTA format
 # --db: Path to the output DIAMOND database file.
-$diamond_location makedb --in $file --db /share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019
+$diamond_location makedb --in $cazydb --db /share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019
+
+python /share/lemaylab-backedup/milklab/michelle/functional_db_comparison/Code/python_scripts/get_all_families_mlt.py -db $cazydb -O /share/lemaylab-backedup/databases/CAZy/CAZy_family_names.tsv
+
+python /share/lemaylab-backedup/Zeya/scripts/ARG_metagenome/get_all_CAZy_ids.py -db $cazydb -O /share/lemaylab-backedup/databases/CAZy/CAZy_id.tsv
+
+# calculate the length of the amino acid sequences
+awk '/^>/{if (l!="") print l; print; l=0; next}{l+=length($0)}END{print l}' $cazydb | paste - - | sed 's/>//g' >> /share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.len.txt
+
+
+### I found that there are duplicate reads in the CAZy database during the RPKG analysis 
+## remove duplicated reads by exact match only 
+##module load java bbmap
+##dedupe.sh in=$cazydb out=/share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019_dedupe.fa ac=f outd=duplicates.fa
+###SHOULD NOT USE DEDUPE.SH BECAUSE IT BIOLOGICALLY POSSIBLE THAT SOME OF THE SEQUENCES ARE THE SAME BETWEEN ORGANISMS
+###INSTEAD, I SHOULD USE METHOD TO REMOVE BASED ON HEADER ONLY
+
+# get the list of all headers
+grep ">" $cazydb > /share/lemaylab-backedup/databases/CAZy/all_headers.txt
+# get the list of repeated headers
+sort /share/lemaylab-backedup/databases/CAZy/all_headers.txt | uniq -d > /share/lemaylab-backedup/databases/CAZy/repeated_headers.txt
+# get uniq headers (keep only one copy of the repeated headers)
+sort /share/lemaylab-backedup/databases/CAZy/all_headers.txt | uniq > /share/lemaylab-backedup/databases/CAZy/unique_headers.txt
+
+# https://www.biostars.org/p/230686/
+module load java bbmap
+dedupe.sh in=$cazydb out=/share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.uniq.fa ac=f requirematchingnames
+
+# re-calculate length
+awk '/^>/{if (l!="") print l; print; l=0; next}{l+=length($0)}END{print l}' /share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.uniq.fa | paste - - | sed 's/>//g' >> /share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.uniq.len.txt
+
+# re-make database
+$diamond_location makedb --in /share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.uniq.fa --db /share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.uniq
