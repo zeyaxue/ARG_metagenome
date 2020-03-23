f2c1e2f7fede4ed00fdc4ebd1028fc634d6b9098
zeyaxue <zhxue@ucdavis.edu>
2020-03-23 12:21:03 -0700

    Make uniq/non-redundant db"

--- init/install_CAZy_db.sh
+++ init/install_CAZy_db.sh
@@ -41,6 +41,8 @@ sort /share/lemaylab-backedup/databases/CAZy/all_headers.txt | uniq -d > /share/
 sort /share/lemaylab-backedup/databases/CAZy/all_headers.txt | uniq > /share/lemaylab-backedup/databases/CAZy/unique_headers.txt
 
 # https://www.biostars.org/p/230686/
+# https://jgi.doe.gov/data-and-tools/bbtools/bb-tools-user-guide/dedupe-guide/
+# https://github.com/BioInfoTools/BBMap/blob/master/sh/dedupe.sh
 module load java bbmap
 dedupe.sh in=$cazydb out=/share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.uniq.fa ac=f requirematchingnames
 
@@ -49,3 +51,7 @@ awk '/^>/{if (l!="") print l; print; l=0; next}{l+=length($0)}END{print l}' /sha
 
 # re-make database
 $diamond_location makedb --in /share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.uniq.fa --db /share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.uniq
+
+# Download the non-redundant version of the database to compare with my own uniq read version
+# https://github.com/linnabrown/run_dbcan/issues/35
+wget http://bcb.unl.edu/dbCAN2/download/CAZyDB.07312019.fa.nr
