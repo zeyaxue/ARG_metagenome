b42b93accb13e67d8c160582d09f27fa22babcaf
zeyaxue <zhxue@ucdavis.edu>
2020-03-23 12:24:02 -0700

    Use updated CAZy db

--- non_pipeline/CAZy_diamond.sh
+++ non_pipeline/CAZy_diamond.sh
@@ -2,7 +2,7 @@
 
 # DIAMOND
 diamond_location=/share/lemaylab-backedup/milklab/programs/diamond
-db=/share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.dmnd
+db=/share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.uniq.dmnd
 
 ####################################################################
 #
@@ -21,3 +21,28 @@ do
 	$diamond_location blastx --db $db -q $file -a $output_dir_diamond/${STEM}.daa -t ./ -k 1 --sensitive --evalue 1e-25
 	$diamond_location view --daa $output_dir_diamond/${STEM}.daa -o $output_dir_diamond/${STEM}.txt -f tab	
 done
+
+for file in $output_dir_diamond/*.daa
+do	
+	STEM=$(basename "$file" .daa)
+
+	echo "counting sample ${STEM}"
+
+	python /share/lemaylab-backedup/Zeya/scripts/ARG_metagenome/non_pipeline/CAZy_db_analysis_counter.py --infile $output_dir_diamond/${STEM}.txt --output $output_dir_diamond/${STEM}_org_by_gene.csv
+
+	#python /share/lemaylab-backedup/Zeya/scripts/ARG_metagenome/merge_organized_diamond_tab.py \
+	#--in $output_dir_diamond/${STEM}_id_org.txt \
+	#--out $output_dir_diamond/${STEM}_id_org_samid.txt \
+	#--mergein $file # This argument does not do anything here, but python2 requires *args to be not empty so I supply a dummy argument 
+
+	#python /share/lemaylab-backedup/Zeya/scripts/ARG_metagenome/merge_organized_diamond_tab.py \
+	#--mergeout $output_dir_diamond/merged_id_diamond_tab.csv \
+	#--mergein $output_dir_diamond/*_id_org_samid.txt
+
+	python /share/lemaylab-backedup/Zeya/scripts/ARG_metagenome/non_pipeline/make_CAZy_normtab.py --mc /share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step5_MicrobeCensus_merged/${STEM}_mc.txt --genelen /share/lemaylab-backedup/databases/CAZy/CAZyDB.07312019.uniq.len.txt --count $output_dir_diamond/${STEM}_org_by_gene.csv --out $output_dir_diamond/${STEM}_org_by_gene_norm.csv --mergein $file # This argument does not do anything here, but python2 requires *args to be not empty so I supply a dummy argument (if using python3, can omit this input)
+done
+
+python /share/lemaylab-backedup/Zeya/scripts/ARG_metagenome/non_pipeline/make_CAZy_normtab.py --mergeout $output_dir_diamond/merged_gene_norm_tab.csv --mergein $output_dir_diamond/*_org_by_gene_norm.csv 
+
+
+echo "CAZy alignment DONE AT: "; date
