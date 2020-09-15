d5c4ea1a7015f3d5afbe3ea50d0e2834db891635
zeyaxue <zhxue@ucdavis.edu>
2020-09-14 22:35:57 -0700

    Updated analysis

--- non_pipeline/kraken2.sh
+++ non_pipeline/kraken2.sh
@@ -1,32 +1,33 @@
 #!/bin/bash 
 
-kraken2_location=/share/lemaylab-backedup/milklab/programs/kraken2-2.0.9b/kraken2_ver2
+kraken2_location=/share/lemaylab-backedup/milklab/programs/kraken2-2.0.9b/kraken2
 bracken_location=/share/lemaylab-backedup/milklab/programs/Bracken-2.6.0
 db=/share/lemaylab-backedup/databases/kraken2-bact-arch-fungi
 
-dup_outdir=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq112/step3_fastuniq
-#mkdir /home/AMR_metagenome/processed_data/Novaseq_072rerun/kraken2
+dup_outdir=..
+mkdir /share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq112/kraken2_ver2
 kraken2_outdir=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq112/kraken2_ver2
 
 # set an empty string variable to get all the names of samples
 names='' 
 
-for file in $dup_outdir/*_R1_dup.fastq
+for file in $dup_outdir/*_R1_paired.fastq
 do
-	STEM=$(basename "$file" _R1_dup.fastq)
+	STEM=$(basename "$file" _R1_paired.fastq )
 	echo "processing sample $STEM"
 
+	# set names parameter for Bracken
 	names+=$STEM
 	names+=','
 
-	file2=$dup_outdir/${STEM}_R2_dup.fastq
+	file2=$trim_outdir/${STEM}_R2_paired.fastq
 
-	$kraken2_location --db $db --threads 35 --confidence 0.2 \
-	 --report $kraken2_outdir/${STEM}_report \
-	 --report-zero-counts --paired $file $file2 > $kraken2_outdir/${STEM}_kraken2.out 
+	#$kraken2_location --db $db --threads 35 --confidence 0.2 \
+	#--report $kraken2_outdir/${STEM}_report \
+	#--report-zero-counts --paired $file $file2 > $kraken2_outdir/${STEM}_kraken2.out 
 
 	# Run Bracken for Abundance Estimation
-	python $bracken_location/src/est_abundance.py -i $kraken2_outdir/${STEM}_report -k $db/database151mers.kmer_distrib -l S -t 10 -o $kraken2_outdir/${STEM}_species_abundance.tsv
+	python2 $bracken_location/src/est_abundance.py -i $kraken2_outdir/${STEM}_report -k $db/database151mers.kmer_distrib -l G -t 10 -o $kraken2_outdir/${STEM}_genus_abundance.tsv
 
 done
 
@@ -34,6 +35,6 @@ done
 names=${names::-1}
 
 # combine all the Bracken output files to a single file
-python $bracken_location/analysis_scripts/combine_bracken_outputs_re.py --names $names --output $kraken2_outdir/merged_species_abundance.tsv --files $kraken2_outdir/*_species_abundance.tsv
+python2 $bracken_location/analysis_scripts/combine_bracken_outputs_re.py --names $names --output $kraken2_outdir/merged_genus_abundance.tsv --files $kraken2_outdir/*_genus_abundance.tsv
 
 echo "KRAKEN2 DONE AT: "; date
