b108128ba2a8b60b21bf45dbfeaccb3d45cd79bf
zeyaxue <zhxue@ucdavis.edu>
2020-11-29 22:29:40 -0800

    update to new CAT version

--- method_dev/step10_taxaid_CAT.sh
+++ method_dev/step10_taxaid_CAT.sh
@@ -6,14 +6,17 @@
 
 echo "NOW STARTING TAXONOMY ID AT: "; date
 
+run_dir=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043
+
 # set input and output file paths
-aln_outdir=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step9_contig_bwa_nomerg
-mkdir /share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step10_CAT_nomerg
-CAT_outdir=/share/lemaylab-backedup/Zeya/proceesed_data/NovaSeq043/step10_CAT_nomerg
+aln_outdir=$run_dir/step9_contig_bwa_nomerg
+mkdir $run_dir/step10_CAT_nomerg
+CAT_outdir=$run_dir/step10_CAT_nomerg
 cd $CAT_outdir 
 
 # Make sure bash knows where to look for softwares 
 CAT=/share/lemaylab-backedup/milklab/programs/CAT-5.0.3/CAT_pack/CAT
+CAT_db=/share/lemaylab-backedup/databases/CAT_prepare_20200618
 progdigal=/software/prodigal/2.6.3/x86_64-linux-ubuntu14.04/bin/prodigal 
 diamond=/share/lemaylab-backedup/milklab/programs/diamond
 
@@ -25,9 +28,9 @@ do
 	# for help /share/lemaylab-backedup/milklab/programs/CAT-5.0.3/CAT_pack/CAT contigs -h
 	# https://github.com/dutilh/CAT
 
-	$CAT contigs -c $file -d /share/lemaylab-backedup/milklab/database/CAT_prepare_20190719/2019-07-19_CAT_database -t /share/lemaylab-backedup/milklab/database/CAT_prepare_20190719/2019-07-19_taxonomy --path_to_prodigal $progdigal --path_to_diamond $diamond -o $CAT_outdir/${STEM}_CAT -n 25
+	$CAT contigs -c $file -d $CAT_db/2020-06-18_CAT_database -t $CAT_db/2020-06-18_taxonomy --path_to_prodigal $progdigal --path_to_diamond $diamond -o $CAT_outdir/${STEM}_CAT -n 25
 
-	$CAT add_names -i $CAT_outdir/${STEM}_CAT.contig2classification.txt -o $CAT_outdir/${STEM}.taxaid.txt -t /share/lemaylab-backedup/milklab/database/CAT_prepare_20190719/2019-07-19_taxonomy --only_official
+	$CAT add_names -i $CAT_outdir/${STEM}_CAT.contig2classification.txt -o $CAT_outdir/${STEM}.taxaid.txt -t $CAT_db/2020-06-18_taxonomy --only_official
 
 	$CAT summarise -c $file -i $CAT_outdir/${STEM}.taxaid.txt -o $CAT_outdir/${STEM}.taxaname.txt
 done	
