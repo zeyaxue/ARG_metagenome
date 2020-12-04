30613fe052e9c9f3812925651882ac552772a1a6
zeyaxue <zhxue@ucdavis.edu>
2020-12-03 22:44:04 -0800

    modify diamond version

--- method_dev/step10_taxaid_CAT.sh
+++ method_dev/step10_taxaid_CAT.sh
@@ -17,8 +17,8 @@ cd $CAT_outdir
 # Make sure bash knows where to look for softwares 
 CAT=/share/lemaylab-backedup/milklab/programs/CAT-5.0.3/CAT_pack/CAT
 CAT_db=/share/lemaylab-backedup/databases/CAT_prepare_20200618
-progdigal=/software/prodigal/2.6.3/x86_64-linux-ubuntu14.04/bin/prodigal 
-diamond=/share/lemaylab-backedup/milklab/programs/diamond
+#progdigal=/software/prodigal/2.6.3/x86_64-linux-ubuntu14.04/bin/prodigal 
+diamond=/share/lemaylab-backedup/milklab/programs/diamond-0.9.34/diamond
 
 for file in $aln_outdir/*_contig_ARGread_aln_mappedonly.fa
 do
@@ -28,7 +28,8 @@ do
 	# for help /share/lemaylab-backedup/milklab/programs/CAT-5.0.3/CAT_pack/CAT contigs -h
 	# https://github.com/dutilh/CAT
 
-	$CAT contigs -c $file -d $CAT_db/2020-06-18_CAT_database -t $CAT_db/2020-06-18_taxonomy --path_to_prodigal $progdigal --path_to_diamond $diamond -o $CAT_outdir/${STEM}_CAT -n 25
+	#$CAT contigs -c $file -d $CAT_db/2020-06-18_CAT_database -t $CAT_db/2020-06-18_taxonomy --path_to_prodigal $progdigal --path_to_diamond $diamond -o $CAT_outdir/${STEM}_CAT -n 25
+	$CAT contigs -c $file -d $CAT_db/2020-06-18_CAT_database -t $CAT_db/2020-06-18_taxonomy -p ${STEM}_CAT.predicted_proteins.faa -a ${STEM}_CAT.predicted_proteins.gff --path_to_diamond $diamond -o $CAT_outdir/${STEM}_CAT -n 25
 
 	$CAT add_names -i $CAT_outdir/${STEM}_CAT.contig2classification.txt -o $CAT_outdir/${STEM}.taxaid.txt -t $CAT_db/2020-06-18_taxonomy --only_official
 
