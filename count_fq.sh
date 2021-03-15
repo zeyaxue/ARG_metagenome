f1de6fc1723e518706d932296554b1cf9aeab273
zeyaxue <zhxue@ucdavis.edu>
2021-03-14 18:15:49 -0700

    update for raw reads

--- count_fq.sh
+++ count_fq.sh
@@ -18,7 +18,7 @@ elif [ "$1" = "gz" ]
 then
 	for file in $2/*.fastq.gz
 	do 
-		STEM=$(basename "$file" .fastq.gz )
+		STEM=$(basename "$file" _R1_001.fastq.gz )
 		echo ${STEM} >> $2/count_fq_log.txt	
 	 
 		echo $(zcat $file|wc -l)/4| >> $2/count_fq_log.txt
@@ -30,17 +30,17 @@ fi
 # organize file
 cd $2
 
-# Make sampleID column
-## I tried over several hours to wrangle the data in variables but that did not work
-awk 'NR%2==1' $2/count_fq_log.txt > $2/col1a.txt  
-# use $() to denote everthing between () are excutable commands
-$(echo sampleID > $2/col1b.txt) # column header
-$(sed 's/_.*//' $2/col1a.txt >> $2/col1b.txt) # need the . period symbol for wildcard to work
-
-# Make read column
-$(echo lib_size > $2/col2.txt)
-awk 'NR%2==0' $2/count_fq_log.txt >> $2/col2.txt
-
-# Combine 2 columns to final stats file
-$(paste $2/col1b.txt $2/col2.txt -d "\t" > $3)
-
+## # Make sampleID column
+## ## I tried over several hours to wrangle the data in variables but that did not work
+## awk 'NR%2==1' $2/count_fq_log.txt > $2/col1a.txt  
+## # use $() to denote everthing between () are excutable commands
+## $(echo sampleID > $2/col1b.txt) # column header
+## $(sed 's/_.*//' $2/col1a.txt >> $2/col1b.txt) # need the . period symbol for wildcard to work
+## 
+## # Make read column
+## $(echo lib_size > $2/col2.txt)
+## awk 'NR%2==0' $2/count_fq_log.txt >> $2/col2.txt
+## 
+## # Combine 2 columns to final stats file
+## $(paste $2/col1b.txt $2/col2.txt -d "\t" > $3)
+## 
